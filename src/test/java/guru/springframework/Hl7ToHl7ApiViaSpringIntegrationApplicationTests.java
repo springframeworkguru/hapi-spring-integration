b3863659bc7414076a529055ed4c20b95ce19e41
John Thompson <john@springframework.guru>
2016-04-27 13:44:32 -0400

    init add of project

---
+++ src/test/java/guru/springframework/Hl7ToHl7ApiViaSpringIntegrationApplicationTests.java
@@ -0,0 +1,16 @@
+package guru.springframework;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = Hl7ToHl7ApiViaSpringIntegrationApplication.class)
+public class Hl7ToHl7ApiViaSpringIntegrationApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
