afe19e3e7fb72a1bf3857a55e93dcbaf97ef8475
Druixo <william.ott@passporthealth.com>
2016-04-28 12:45:22 -0500

    Adding in HL7 v2.1 Service, Publisher, and Parser.

--- src/test/java/guru/springframework/hl7/publish/HL7v21MessageServiceImplTest.java
+++ src/test/java/guru/springframework/hl7/publish/HL7v21MessageServiceImplTest.java
@@ -9,16 +9,17 @@ import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.jms.core.JmsTemplate;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import javax.jms.*;
+import javax.jms.ConnectionFactory;
+import javax.jms.Queue;
 
 /**
  * Created by jt on 4/27/16.
  */
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(Hl7ToHl7ApiViaSpringIntegrationApplication.class)
-public class HL7MessageServiceImplTest {
+public class HL7v21MessageServiceImplTest {
 
-    String msg = "MSH|^~\\&|HIS|RIH|EKG|EKG|199904140038||ADT^A01||P|2.2\r"
+    String msg = "MSH|^~\\&|HIS|RIH|EKG|EKG|199904140038||ADT^A01||P|2.1\r"
                      + "PID|0001|00009874|00001122|A00977|SMITH^JOHN^M|MOM|19581119|F|NOTREAL^LINDA^M|C|564 SPRING ST^^NEEDHAM^MA^02494^US|0002|(818)565-1551|(425)828-3344|E|S|C|0000444444|252-00-4414||||SA|||SA||||NONE|V1|0001|I|D.ER^50A^M110^01|ER|P00055|11B^M011^02|070615^BATMAN^GEORGE^L|555888^NOTREAL^BOB^K^DR^MD|777889^NOTREAL^SAM^T^DR^MD^PHD|ER|D.WT^1A^M010^01|||ER|AMB|02|070615^NOTREAL^BILL^L|ER|000001916994|D||||||||||||||||GDD|WA|NORM|02|O|02|E.IN^02D^M090^01|E.IN^01D^M080^01|199904072124|199904101200|199904101200||||5555112333|||666097^NOTREAL^MANNY^P\r"
                      + "NK1|0222555|NOTREAL^JAMES^R|FA|STREET^OTHER STREET^CITY^ST^55566|(222)111-3333|(888)999-0000|||||||ORGANIZATION\r"
                      + "PV1|0001|I|D.ER^1F^M950^01|ER|P000998|11B^M011^02|070615^BATMAN^GEORGE^L|555888^OKNEL^BOB^K^DR^MD|777889^NOTREAL^SAM^T^DR^MD^PHD|ER|D.WT^1A^M010^01|||ER|AMB|02|070615^VOICE^BILL^L|ER|000001916994|D||||||||||||||||GDD|WA|NORM|02|O|02|E.IN^02D^M090^01|E.IN^01D^M080^01|199904072124|199904101200|||||5555112333|||666097^DNOTREAL^MANNY^P\r"
@@ -35,12 +36,12 @@ public class HL7MessageServiceImplTest {
 
     @Autowired
     @Qualifier("jmsConnectionFactory")
-    public void setConnectionFactory(javax.jms.ConnectionFactory connectionFactory) {
+    public void setConnectionFactory(ConnectionFactory connectionFactory) {
         this.connectionFactory = connectionFactory;
     }
 
     @Autowired
-    @Qualifier("hl7v22PublisherService")
+    @Qualifier("hl7v21PublisherService")
     public void setHl7MessageService(HL7MessagePublisher hl7MessageService) {
         this.hl7MessageService = hl7MessageService;
     }
