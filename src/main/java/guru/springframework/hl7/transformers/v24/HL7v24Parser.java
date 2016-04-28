54369b1a4ebed51e63cdf21f443b3e92a7864d93
michael.kirton <michael.kirton@passporthealth.com>
2016-04-28 14:23:52 -0500

    Changed HL7Parsers to implement interface and extend a base abstract class to consolidate duplicate code.

--- src/main/java/guru/springframework/hl7/transformers/v24/HL7v24Parser.java
+++ src/main/java/guru/springframework/hl7/transformers/v24/HL7v24Parser.java
@@ -1,20 +1,9 @@
 package guru.springframework.hl7.transformers.v24;
 
-import ca.uhn.hl7v2.DefaultHapiContext;
-import ca.uhn.hl7v2.HL7Exception;
-import ca.uhn.hl7v2.HapiContext;
-import ca.uhn.hl7v2.model.Message;
-import ca.uhn.hl7v2.parser.Parser;
+import guru.springframework.hl7.transformers.HL7Parser;
+import guru.springframework.hl7.transformers.HL7ParserBase;
 import org.springframework.stereotype.Component;
 
 @Component
-public class HL7v24Parser {
-
-    public Message parseMessage(String message) throws HL7Exception {
-        HapiContext context = new DefaultHapiContext();
-
-        Parser p = context.getGenericParser();
-
-        return p.parse(message);
-    }
+public class HL7v24Parser extends HL7ParserBase implements HL7Parser {
 }
