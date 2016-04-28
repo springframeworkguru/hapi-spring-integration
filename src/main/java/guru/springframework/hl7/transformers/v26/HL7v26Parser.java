ac33123a4424d9b67ab1c846479b30624ae7b152
Druixo <william.ott@passporthealth.com>
2016-04-28 13:24:59 -0500

    Added v26

---
+++ src/main/java/guru/springframework/hl7/transformers/v26/HL7v26Parser.java
@@ -0,0 +1,23 @@
+package guru.springframework.hl7.transformers.v26;
+
+import ca.uhn.hl7v2.DefaultHapiContext;
+import ca.uhn.hl7v2.HL7Exception;
+import ca.uhn.hl7v2.HapiContext;
+import ca.uhn.hl7v2.model.Message;
+import ca.uhn.hl7v2.parser.Parser;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by william.ott on 4/28/2016.
+ */
+@Component
+public class HL7v26Parser {
+
+    public Message parseMessage(String message) throws HL7Exception {
+        HapiContext context = new DefaultHapiContext();
+
+        Parser p = context.getGenericParser();
+
+        return p.parse(message);
+    }
+}
